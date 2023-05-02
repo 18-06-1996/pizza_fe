6cc08c0469400aa5963ef2b8eee248477d72a8df
18-06-1996 <karthick18696@gmail.com>
2023-05-02 21:36:34 +0530

    change

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react'
 import { useDispatch,useSelector } from 'react-redux'
-// import pizzas  from '../pizzadata.js'
+
 import Pizza from '../components/Pizza'
 import { getAllPizzas } from '../actions/pizzaActions'
 const Homescreen = () => {
@@ -19,13 +19,13 @@ useEffect(()=>{
       {
         loading ?(<h3> Loading</h3>): error ? (<h3>somthing went wrong</h3>) :(
 
-          pizzas.map(pizza=>{
+        pizzas.map((pizza)=>{
             return <div className='col-md-4' key={pizza._id}>
-                <div >
-                   <Pizza pizza={pizza}/>
-                </div>
-            </div>
-        })
+                        <div >
+                          <Pizza pizza={pizza}/>
+                        </div>
+                    </div> 
+                  })
 
         )
       }
